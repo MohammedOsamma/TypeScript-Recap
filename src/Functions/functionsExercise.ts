fe037010e39a80475da847441703660b853629b4
MohammedOsamma <mo856915i@gmail.com>
2024-10-16 12:50:21 +0200

    Variables , Functions and Objects Type

---
+++ src/Functions/functionsExercise.ts
@@ -0,0 +1,29 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Write a function called "twoFer" that accepts a person's name
+// It should return a string in the format "one for <name>, one for me"
+// If no name is provided, it should default to "you"
+
+// twoFer() => "One for you, one for me"
+// twoFer("Elton") => "One for Elton, one for me"
+
+const twoFer = (person: string = "You") => {
+  return `Onr For ${person} , One For me`;
+};
+
+console.log(twoFer());
+console.log(twoFer("Mohamed"));
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
+// isLeapYear(2012) => true
+// isLeapYear(2013) => false
+
+const isLeapYear = (year: number): boolean => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+console.log(isLeapYear(2012)); // true
+console.log(isLeapYear(2013)); // false
